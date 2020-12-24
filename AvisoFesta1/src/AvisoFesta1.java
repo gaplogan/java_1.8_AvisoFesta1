3753a70b0fa6be66d56c9abf984641b4e6ef53ee
Gabriel <gaplogan@gmail.com>
2020-12-24 15:50:53 -0300

    2020.12.24.15.50

---
+++ AvisoFesta1/src/AvisoFesta1.java
@@ -0,0 +1,9 @@
+public class AvisoFesta1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hoje vai ter festa na casa da Fabi");
+        System.out.println("Hoje vai ter festa na casa da Fabi");
+        System.out.println("Hoje vai ter festa na casa da Fabi");
+        System.out.println("Hoje vai ter festa na casa da Fabi");
+        System.out.println("Hoje vai ter festa na casa da Fabi");
+    }
+}
